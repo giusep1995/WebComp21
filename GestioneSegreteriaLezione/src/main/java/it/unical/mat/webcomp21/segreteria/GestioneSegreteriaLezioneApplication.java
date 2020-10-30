4ccbd87566bef7c1b9a75ddf3c53168d7f2a9c56
Kristian <kristian.reale@gmail.com>
2020-10-30 08:59:23 +0100

    first commit

---
+++ GestioneSegreteriaLezione/src/main/java/it/unical/mat/webcomp21/segreteria/GestioneSegreteriaLezioneApplication.java
@@ -0,0 +1,12 @@
+package it.unical.mat.webcomp21.segreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneSegreteriaLezioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneSegreteriaLezioneApplication.class, args);
+	}
+}
---
+++ GestioneSegreteriaLezione/src/test/java/it/unical/mat/webcomp21/segreteria/GestioneSegreteriaLezioneApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.mat.webcomp21.segreteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneSegreteriaLezioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
